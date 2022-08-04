a0f6b884499c658ad45916cf3a79d4965b5866ce
Evgeniy <evgen-z2007@yandex.ru>
2022-08-05 01:09:50 +0500

    Rpogram

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+string [] array = new string[5];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Введите элементы массива");
+    array[i] = Console.ReadLine();
+}
+Console.WriteLine();
+Console.WriteLine("Вывод заданных элементов");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + "\t");
+}
+
+Console.WriteLine();
+Console.WriteLine("Вывод элементов, состоящих из 3 символов");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length<=3)
+    Console.Write(array[i] + "\t");
+}
